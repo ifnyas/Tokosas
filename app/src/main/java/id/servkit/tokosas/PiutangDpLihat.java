2d5511686100fd491a1198ab0bb4f55a6d18df32
ifnyas <irfanyasiras@gmail.com>
2019-02-28 10:25:24 +0700

    Commit

--- app/src/main/java/id/servkit/tokosas/PiutangDpLihat.java
+++ app/src/main/java/id/servkit/tokosas/PiutangDpLihat.java
@@ -2,8 +2,11 @@ package id.servkit.tokosas;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 
 public class PiutangDpLihat extends AppCompatActivity {
 
@@ -18,5 +21,31 @@ public class PiutangDpLihat extends AppCompatActivity {
     public void toPiutangDp(View view) {
         Intent intent = new Intent(this, PiutangDp.class);
         startActivity(intent);
+    }
+
+    public void toPiutangDpLihatUbah(View view) {
+        final AlertDialog dialogBuilder = new AlertDialog.Builder(this).create();
+        LayoutInflater inflater = this.getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.dialog_tambahan_ubah, null);
+
+        Button button1 = dialogView.findViewById(R.id.buttonSubmit);
+        Button button2 = dialogView.findViewById(R.id.buttonCancel);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialogBuilder.dismiss();
+            }
+        });
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // DO SOMETHINGS
+                dialogBuilder.dismiss();
+            }
+        });
+
+        dialogBuilder.setView(dialogView);
+        dialogBuilder.show();
     }
 }
--- app/src/main/java/id/servkit/tokosas/PiutangPpLihat.java
+++ app/src/main/java/id/servkit/tokosas/PiutangPpLihat.java
@@ -2,8 +2,11 @@ package id.servkit.tokosas;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 
 public class PiutangPpLihat extends AppCompatActivity {
 
@@ -18,5 +21,57 @@ public class PiutangPpLihat extends AppCompatActivity {
     public void toPiutangPp(View view) {
         Intent intent = new Intent(this, PiutangPp.class);
         startActivity(intent);
+    }
+
+    public void toPiutangPpLihatPembayaranUbah(View view) {
+        final AlertDialog dialogBuilder = new AlertDialog.Builder(this).create();
+        LayoutInflater inflater = this.getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.dialog_pembayaran_ubah, null);
+
+        Button button1 = dialogView.findViewById(R.id.buttonSubmit);
+        Button button2 = dialogView.findViewById(R.id.buttonCancel);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialogBuilder.dismiss();
+            }
+        });
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // DO SOMETHINGS
+                dialogBuilder.dismiss();
+            }
+        });
+
+        dialogBuilder.setView(dialogView);
+        dialogBuilder.show();
+    }
+
+    public void toPiutangPpLihatPengurangUbah(View view) {
+        final AlertDialog dialogBuilder = new AlertDialog.Builder(this).create();
+        LayoutInflater inflater = this.getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.dialog_pengurang_ubah, null);
+
+        Button button1 = dialogView.findViewById(R.id.buttonSubmit);
+        Button button2 = dialogView.findViewById(R.id.buttonCancel);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialogBuilder.dismiss();
+            }
+        });
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // DO SOMETHINGS
+                dialogBuilder.dismiss();
+            }
+        });
+
+        dialogBuilder.setView(dialogView);
+        dialogBuilder.show();
     }
 }
